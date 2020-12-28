757dbc9e025309fc4d76403e2a5791f05b990ced
V3radux <sebastian.klaffenboeck@gmx.at>
2020-12-28 12:55:57 +0100

    Aufteilung der php-files

--- hinfahrt.php
+++ hinfahrt.php
@@ -5,13 +5,8 @@ $list = [
         'start_station' => 'Waizenkirchen Bahnhof (Vorplatz)',
         'start_time' => '07:00',
         'end_station' => 'Grieskirchen Schulzentrum',
-        'end_time' => '07:44'
-    ],
-    [
-        'start_station' => 'Aegidi',
-        'start_time' => '06:00',
-        'number' => 5,
-        'boolean' => false
+        'end_time' => '07:44',
+        'lines_used' => '692'
     ]
 ];
 
